7357e06879c84e33e854975fe173c9a063a45ec0
Gilad Rave <gilad@embryonics.me>
2022-05-18 10:32:28 +0300

    ignore case

--- hide.sh
+++ hide.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 secret_json="${1}"
 key_pattern="${2}"
+ignore_case="${3}"
 echo "secret_json='${secret_json}'"
 echo "key_pattern='${key_pattern}'"
+echo "ignore_case='${ignore_case}'"
+if [[ "${ignore_case,,}" =~ ^1|y(es)?|t(rue)?$ ]]
+then
+    echo nocasematch
+    shopt -qs nocasematch
+fi
 secret_keys=($(echo ${secret_json} | jq -r 'keys[]'))
 for secret_key in "${secret_keys[@]}"
 do
