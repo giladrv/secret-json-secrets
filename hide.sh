d83d2adb12a42ae41f603c3843cda1b14636ade9
Gilad Rave <gilad@embryonics.me>
2022-05-18 08:49:04 +0300

    first commit

---
+++ hide.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+secret_json="${1}"
+key_pattern="${2}"
+echo "secret_json=${secret_json}"
+echo "key_pattern=${secret_json}"
+secret_keys=($(echo ${secret_json} | jq -r 'keys[]'))
+for secret_key in "${secret_keys[@]}"
+do
+    secret_value=$( echo "${secret_json}" | jq -r ".${secret_key}" )
+    if [[ "${secret_key}" =~ ${key_pattern} ]]
+    then
+        echo "::add-mask::${secret_value}"
+    fi
+    echo "${secret_key}=${secret_value}"
+done
