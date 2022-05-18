4e8c3d0c32be0174f131a116f00643cca1ba7928
Gilad Rave <gilad@embryonics.me>
2022-05-18 11:13:14 +0300

    extend

--- hide.sh
+++ hide.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+set -euo pipefail
 secret_json="${1}"
 key_pattern="${2}"
-ignore_case="${3}"
-echo "secret_json='${secret_json}'"
-echo "key_pattern='${key_pattern}'"
-echo "ignore_case='${ignore_case}'"
-if [[ "${ignore_case,,}" =~ ^1|y(es)?|t(rue)?$ ]]
+val_pattern="${3}"
+ignore_case="$(echo "${4}" |  tr '[:upper:]' '[:lower:]' )"
+verbose="${5}"
+[[ ${verbose} == '1' ]] && echo "secret_json='${secret_json}'"
+[[ ${verbose} == '1' ]] && echo "key_pattern='${key_pattern}'"
+[[ ${verbose} == '1' ]] && echo "val_pattern='${val_pattern}'"
+[[ ${verbose} == '1' ]] && echo "ignore_case='${ignore_case}'"
+if [[ "${ignore_case}" =~ ^1|y(es)?|t(rue)?$ ]]
 then
-    echo nocasematch
+    [[ ${verbose} == '1' ]] && echo "nocasematch"
     shopt -qs nocasematch
 fi
 secret_keys=($(echo ${secret_json} | jq -r 'keys[]'))
+[[ ${verbose} == '1' ]] && echo "#keys=${#secret_keys[@]}"
 for secret_key in "${secret_keys[@]}"
 do
     secret_value=$( echo "${secret_json}" | jq -r ".${secret_key}" )
-    if [[ "${secret_key}" =~ ${key_pattern} ]]
+    if [[ "${secret_key}" =~ ${key_pattern} || \
+        ! -z "${val_pattern}" && "${secret_value}" =~ ${val_pattern} ]]
     then
         echo "::add-mask::${secret_value}"
     fi
-    echo "${secret_key}='${secret_value}'"
+    [[ ${verbose} == '1' ]] && echo "  ${secret_key}='${secret_value}'"
 done
