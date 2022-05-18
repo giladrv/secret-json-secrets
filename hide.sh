82e14ef6833819fdb8a5fbb69f8723e143ccbe2e
Gilad Rave <gilad@embryonics.me>
2022-05-18 09:32:20 +0300

    debug

--- hide.sh
+++ hide.sh
@@ -2,11 +2,14 @@
 secret_json="${1}"
 key_pattern="${2}"
 echo "secret_json=${secret_json}"
-echo "key_pattern=${secret_json}"
+echo "key_pattern=${key_pattern}"
 secret_keys=($(echo ${secret_json} | jq -r 'keys[]'))
+echo "got keys"
 for secret_key in "${secret_keys[@]}"
 do
+    echo "secret_key=${secret_key}"
     secret_value=$( echo "${secret_json}" | jq -r ".${secret_key}" )
+    echo "secret_value=${secret_value}"
     if [[ "${secret_key}" =~ ${key_pattern} ]]
     then
         echo "::add-mask::${secret_value}"
